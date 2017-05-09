2baf092738c1d842cd46e4eb21d92f9d6818a330
Piotr <p.maruszewski@outlook.com>
2017-05-10 01:26:05 +0200

    qwert

--- AnkietaAlkoholowa/App_Start/BundleConfig.cs
+++ AnkietaAlkoholowa/App_Start/BundleConfig.cs
@@ -13,7 +13,7 @@ namespace AnkietaAlkoholowa
 
 
             bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include("~/Scripts/bootstrap.min.js",
-                 "~/Scripts/bootstrap.js", "~/Scripts/Chart.js"));
+                 "~/Scripts/bootstrap.js", "~/Scripts/Chart.js","~/Scripts/canvasjs.min.js"));
             bundles.Add(new ScriptBundle("~/bundles/jquery").Include("~/Scripts/jquery-1.9.1.min.js","~/Scripts/jquery-ui.js"));
           
 
